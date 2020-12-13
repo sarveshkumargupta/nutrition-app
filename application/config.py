07d210d2323060ad2dd955dcdccdf497ac6f2d34
sarveshkumargupta <sarveshkumargupta@hotmail.com>
2020-12-13 14:19:26 +0530

    added secret key thorugh env var

--- application/config.py
+++ application/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     DEBUG = True
-    SECRET_KEY = 'Sarvesh@123'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     BOOTSTRAP_BTN_STYLE = 'btn px-4 btn-outline-success'
 
     APP_ID = os.getenv('APP_ID')
